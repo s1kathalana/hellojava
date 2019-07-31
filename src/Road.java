96b59148f355c8fd6cc3808051514979101b0171
s1kathalana <s1kathalana@gmail.com>
2019-07-31 18:52:20 +0100

    cb

---
+++ src/Road.java
@@ -0,0 +1,6 @@
+public class Road {
+    public static void main (String args[]){
+
+        System.out.println("sk");
+    }
+}
---
+++ src/Tv.java
@@ -0,0 +1,6 @@
+public class Tv {
+    public static void main (String args[]){
+
+        System.out.println("sunil");
+    }
+}
