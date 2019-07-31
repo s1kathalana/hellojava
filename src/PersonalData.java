d571d770e9df101a65ead3111a4f3818ebadabda
s1kathalana <s1kathalana@gmail.com>
2019-07-31 17:34:24 +0100

    personalinformation

---
+++ src/PersonalData.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class PersonalData {
+    public static void main(String args[]){
+
+        System.out.println("Name:Sunil");
+        System.out.println("Age:32");
+        System.out.println("Address:6 summerlea slough SL1 2XR");
+        System.out.println("Mobile no:01345678935");
+        System.out.println("Salary:15000 GBP per annum");
+    }
+
+
+}
---
+++ src/PersonalInformation.java
@@ -0,0 +1,16 @@
+public class PersonalInformation {
+
+    public static void main (String args[]){
+
+         String name = "Sunil";
+         int age = 32;
+         String Address = "6,summerlea slough SL12YH";
+         long Mobile = 1234567892;
+         double Salary = 1004.36;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(Address);
+        System.out.println(Mobile);
+        System.out.println(Salary);
+    }
+}
